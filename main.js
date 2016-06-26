37a91eacd3cb550233f0ddfaa2d01178989de17c
Slowhand0309 <slowhand0309@gmail.com>
2016-06-26 09:44:54 +0900

    Updated api callback

--- main.js
+++ main.js
@@ -19,28 +19,41 @@ var Main = {
     // Info button.
     $('#info').bind('click', function() {
       console.log("click [info]");
-      Main.get('http://localhost:9292/api/info');
+      Main.get('http://localhost:9292/api/info',
+        function(data) {
+          $('#info_msg .list .version span').text(data.version);
+          $('#info_msg .list .methods span').text(data.methods.join(','));
+          $('#info_msg .list .link a').text(data.link);
+        }
+      );
     });
 
     // Ping button.
     $('#ping').bind('click', function() {
       console.log("click [ping]");
-      Main.get('http://localhost:9292/api/ping');
+      var url = 'http://localhost:9292/api/ping';
+      var echo = $('#ping_echo input').val();
+      if (echo != '') {
+        url += '?echo=' + echo;
+      }
+      Main.get(url, function(data) {
+        $('#ping_msg p').text(data.ping);
+      });
     });
   },
 
   /**
    * Get json data.
    */
-  get: function(url) {
+  get: function(url, onSuccess) {
 
     $.ajax({
       type: 'GET',
       url: url,
       dataType: 'json',
       success: function(data){
-        var jsonData = $.parseJSON(data);
-        console.table(jsonData);
+        console.log(data);
+        onSuccess(data);
       }
     });
   }
