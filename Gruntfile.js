2f3584e0e81ac9c29f8f879b4c63b6d7e5ecdba1
Slowhand0309 <slowhand0309@gmail.com>
2016-09-02 00:04:07 +0900

    Branch phonegap develop

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Load npm tasks.
+  grunt.loadNpmTasks('phonegap-grunt');
+
+  // Configuration.
+  grunt.initConfig({
+
+    // phonegap-grunt config.
+    pg: {
+      // Project create.
+      init: {
+        path: '.',
+        id: 'ekibito.com',
+        name: 'ekibito'
+      },
+      // Platform add android and ios.
+      add: {
+        platform: ['android', 'ios']
+      },
+      // Build android and ios platforms.
+      build: {
+        platform: 'ios'
+      },
+      // Run android platform.
+      run: {
+        platform: 'ios'
+      },
+      // Platform rm & platform add.
+      clean: {
+        platform: ['android', 'ios']
+      }
+    }
+  });
+};
--- index.android.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-
-import ekibito from './src/ekibito';
-
-AppRegistry.registerComponent('ekibito', () => ekibito);
